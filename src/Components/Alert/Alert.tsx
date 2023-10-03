ffe12f971f32da22d1d4e88d4898f47e255aff65
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-10-03 16:13:44 +0300

    upd to ts

--- src/Components/Alert/Alert.tsx
+++ src/Components/Alert/Alert.tsx
@@ -1,12 +1,11 @@
 //всплывающая подсказка об добавлении товара
-
-import React, { useEffect, useContext } from "react";
+import React from "react";
 import { ShopContext } from "../../context";
 import styles from "./Alert.module.scss";
 
-const Alert = () => {
-  const { alertName, closeAlert } = useContext(ShopContext);
-  useEffect(() => {
+const Alert = (): JSX.Element => {
+  const { alertName, closeAlert } = React.useContext(ShopContext);
+  React.useEffect(() => {
     const timerId = setTimeout(closeAlert, 3000);
     return () => clearTimeout(timerId);
     //eslint-disable-next-line
